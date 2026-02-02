15a53a8e43944adcf24d8ec4236b99e2daef011c
CGSLV <caiogracasilva@gmail.com>
2026-02-02 15:49:07 -0400

    Estruturas de Repetição em JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+//
+
+var i = 0
+while (i < 6) {
+  console.log(`Passo ${i}`)
+  i++ // == i += 1 == i = i + 1
+}
+
+console.log()
+i = 0
+do {
+    console.log(`Passo ${i}`)
+    i++
+} while (i < 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+// var c = 1
+// while (c <= 10) {
+//   console.log(c)
+//   c++
+// }
+
+// ----------------------------
+//Reescrevendo em FOR
+
+for (var c = 1;c <= 10;c++) {
+  console.log(c)
+}
